42ba33b95c846984c84f22ec62f9d35d9a23f776
franzkenzo <franzkenzo17@gmail.com>
2022-10-12 00:31:50 +0700

    initial commit

---
+++ app/Views/home.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">Apolen</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Promo</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Herbal</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Generik</a>
+        </li>
+      </ul>
+      <a href="" class="ms-auto btn btn-primary">Hubungi Kami</a>
+    </div>
+  </div>
+</nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+            <div class="card border-0">
+    <img src="../asset/herocyn.jpeg" class="card-img-top" alt="...">
+    <div class="text-center card-body">
+    <h5 class="card-title">Bedak Herocyn</h5>
+    <p class="card-text"> HEROCYN BEDAK KULIT 85 G merupakan bedak yang digunakan untuk membantu meredakan biang kerngat dan gatal-gatal pada kulit.</p>
+    <a href="#" class=" btn btn-primary">RP 20.000</a>
+  </div>
+</div></div>
+            <div class="col-lg-3"></div>
+            <div class="col-lg-3"></div>
+            <div class="col-lg-3"></div>
+        </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+  
+</html>
